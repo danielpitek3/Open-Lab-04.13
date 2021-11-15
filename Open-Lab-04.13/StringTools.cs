279680f1052e9370198df5fe0415dd61bfbf7c9a
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-15 18:34:50 +0100

    done

--- Open-Lab-04.13/StringTools.cs
+++ Open-Lab-04.13/StringTools.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._13
 {
     public class StringTools
     {
-        public string GetLongestCommonSequence(string str1, string str2)
+
+        public string GetLongestCommonSequence(string s1, string s2)
         {
-            throw new NotImplementedException();
+            char[] str1 = s1.ToCharArray();
+            char[] str2 = s2.ToCharArray();
+            int[,] l = new int[str1.Length, str2.Length];
+            int lcs = -1;
+            string substr = string.Empty;
+            int end = -1;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str2.Length; j++)
+                {
+                    if (str1[i] == str2[j])
+                    {
+                        if (i == 0 || j == 0)
+                        {
+                            l[i, j] = 1;
+                        }
+                        else
+                            l[i, j] = l[i - 1, j - 1] + 1;
+                        if (l[i, j] > lcs)
+                        {
+                            lcs = l[i, j];
+                            end = i;
+                        }
+       
+                    }
+                    else
+                        l[i, j] = 0;
+                }
+            }
+
+            for (int i = end - lcs + 1; i <= end; i++)
+            {
+                substr += str1[i];
+            }
+            return substr;
         }
     }
 }
